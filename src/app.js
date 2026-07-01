dcd012777dec28e9d970997b6d198b2c21a5f478
Deepthimanth <fandomdeepthimanth@gmail.com>
2026-07-01 13:05:39 +0530

    added controllers and debugging

--- src/app.js
+++ src/app.js
@@ -21,4 +21,14 @@ app.use(express.urlencoded({
 app.use(express.static("public"))
 app.use(cookieParser())
 
+
+//routers import
+import userRouter from "./routes/user.routes.js"
+
+//routers declaration
+app.use("/api/v1/users", userRouter)
+
+//  http://localhost:8000/api/v1/users/register
+// additional v1 is added to route to make future version like v2 without breaking existing routes
+
 export { app };
--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js"
 
 dotenv.config({
-    path: "./env"
+    path: "./.env"
 });
 
 connectDB()
     .then(() => {
-        app.listen(process.env.port || 8000, () => {
-            console.log(`server is ready at port :${process.env.port}`);
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`server is ready at port :${process.env.PORT}`);
         });
     })
     .catch((error) => {
