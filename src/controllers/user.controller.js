300127c40e5af40b95ffb8c431814ef7cb346c74
Deepthimanth <fandomdeepthimanth@gmail.com>
2026-07-13 21:40:42 +0530

    addded image edits

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -300,4 +300,62 @@ const updateAccountDetails = asyncHandler(async(req,res)=>{
 // it will best to create a separate controller for updating image bcoz if you add it in UpdateAccountDetails,
 // even just u want to update new image it sends text data too everytime so it better to make separate controllers
 
-export { registerUser, loginUser ,logoutUser,refreshAccessToken,changeCurrentPassword,getCurrentUser,updateAccountDetails }
+const updateUserAvatar = asyncHandler(async(req,res)=>{
+    const avatarLocalPath = req.file?.path
+
+    if (!avatarLocalPath) {
+        throw new ApiError(400,"Avatar file is missing")
+    }
+
+    const avatar = await uploadOnClodinary(avatarLocalPath)
+
+    if(!avatar.url) {
+        throw new ApiError(400,"Error while uploading on avatar")
+    }
+
+    const user = await User.findByIdAndUpdate(
+        req.user?._id,
+        {
+            $set:{
+                avatar:avatar.url
+            }
+        },{new:true}
+    ).select("-password")
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200,user,"avatar image updated successfully")
+    )
+})
+
+const updateUserCoverImage = asyncHandler(async(req,res)=>{
+    const coverImageLocalPath = req.file?.path
+
+    if (!coverImageLocalPath) {
+        throw new ApiError(400,"Cover image file is missing")
+    }
+
+    const coverImage = await uploadOnClodinary(avatarLocalPath)
+
+    if(!coverImage.url) {
+        throw new ApiError(400,"Error while uploading on coverimage")
+    }
+
+    const user = await User.findByIdAndUpdate(
+        req.user?._id,
+        {
+            $set:{
+                coverImage:coverImage.url
+            }
+        },{new:true}
+    ).select("-password")
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200,user,"Cover image updated successfully")
+    )
+})
+
+export { registerUser, loginUser ,logoutUser,refreshAccessToken,changeCurrentPassword,getCurrentUser,updateAccountDetails,updateUserAvatar,updateUserCoverImage }
